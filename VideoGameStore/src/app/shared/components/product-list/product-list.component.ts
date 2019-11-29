976511cb5fcd80c58c101abd06bb3f7f137fec94
cbedrosian1 <cbedrosian1@gmail.com>
2019-11-29 03:49:23 -0500

    Fixed carousel on home page and video game list now displays video games

---
+++ VideoGameStore/src/app/shared/components/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../../interfaces/product.interface';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  @Input() products: Product[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
